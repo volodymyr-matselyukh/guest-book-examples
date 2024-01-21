05950ffa477f734685d37f0b4d9ef5b538d9b306
Matias Benary <matiasbenary@gmail.com>
2024-01-21 19:04:19 -0300

    feat: update and reoirganize repo

--- frontend/index.js
+++ frontend/index.js
@@ -7,12 +7,14 @@ import App from './App';
 import { GuestBook } from './near-interface';
 import { Wallet } from './near-wallet';
 
+const CONTRACT_NAME = "guestbook.near-examples.testnet"
+
 // When creating the wallet you can choose to create an access key, so the user
 // can skip signing non-payable methods when talking wth the  contract
-const wallet = new Wallet({ createAccessKeyFor: process.env.CONTRACT_NAME })
+const wallet = new Wallet({ createAccessKeyFor: CONTRACT_NAME})
 
 // Abstract the logic of interacting with the contract to simplify your flow
-const guestBook = new GuestBook({ contractId: process.env.CONTRACT_NAME, walletToUse: wallet });
+const guestBook = new GuestBook({ contractId: CONTRACT_NAME, walletToUse: wallet });
 
 // Setup on page load
 window.onload = async () => {
